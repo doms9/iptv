00000d9c0424632645789101ca63cd57449298a4
doms9 <96013514+doms9@users.noreply.github.com>
2026-09-24 00:12:25 -0400

    e - nhl - misc edits.

--- M3U8/scrapers/xyzstreams.py
+++ M3U8/scrapers/xyzstreams.py
@@ -168,7 +168,7 @@ async def get_events(cached_keys: KeysView[str]) -> list[XYZEvent]:
 
         HTML_FILE.write(events)
 
-    start_ts = now.delta(hours=-3).timestamp()
+    start_ts = now.delta(minutes=-30).timestamp()
     end_ts = now.delta(minutes=30).timestamp()
 
     return [
